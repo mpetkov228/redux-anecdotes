c0b049880c510010c8ce0fb60ef2bcb6d53f23d9
MihailPetkov96 <mpetkov228@gmail.com>
2024-10-15 20:25:52 +0300

    Complete exercise 6.9

--- src/components/AnecdoteList.jsx
+++ src/components/AnecdoteList.jsx
@@ -2,7 +2,16 @@ import { useDispatch, useSelector } from "react-redux";
 import { voteAction } from "../reducers/anecdoteReducer";
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state);
+  const anecdotes = useSelector(({filter, anecdotes}) => {
+    if (filter === '') {
+      return anecdotes;
+    }
+    return anecdotes.filter(anecdote => 
+      anecdote.content
+      .toLowerCase()
+      .includes(filter.toLowerCase())
+    );
+  });
   const dispatch = useDispatch();
 
   const vote = (id) => {
--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,6 +1,12 @@
-const Filter = () => {
-  const handleChange = (event) => {
+import { useDispatch } from "react-redux";
+import { filterChange } from "../reducers/filterReducer";
 
+const Filter = () => {
+  const dispatch = useDispatch();
+
+  const handleChange = (event) => {
+    const filter = event.target.value;
+    dispatch(filterChange(filter));  
   };
 
   const style = {
