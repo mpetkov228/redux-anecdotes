43842935ad63fbfb4f9c749441e6017dae6bec96
MihailPetkov96 <mpetkov228@gmail.com>
2024-10-20 10:14:52 +0300

    Remove unused functions

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -1,14 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const getId = () => (100000 * Math.random()).toFixed(0);
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-};
-
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
   initialState: [],
