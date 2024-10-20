dbd68002470c4f03a795a4bcec40e835512670e5
MihailPetkov96 <mpetkov228@gmail.com>
2024-10-20 10:29:43 +0300

    Move initial anecdote fetching to App.jsx

--- src/store.js
+++ src/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import anecdoteReducer, { setAnecdotes } from './reducers/anecdoteReducer';
+import anecdoteReducer from './reducers/anecdoteReducer';
 import filterReducer from "./reducers/filterReducer";
 import notificationReducer from './reducers/notificationReducer';
-
-import anecdoteService from './services/anecdotes';
 
 const store = configureStore({
   reducer: {
@@ -14,8 +12,5 @@ const store = configureStore({
   }
 });
 
-anecdoteService.getAll().then(anecdotes => {
-  store.dispatch(setAnecdotes(anecdotes));
-});
 
 export default store;
