7f08397bb3a06045fa0a01ab80e8b3e4dbd46d5a
MihailPetkov96 <mpetkov228@gmail.com>
2024-10-21 17:44:01 +0300

    Complete exercise 6.16

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,13 @@ import Notification from './components/Notification';
 import AnecdoteForm from './components/AnecdoteForm';
 import AnecdoteList from './components/AnecdoteList';
 
-import anecdoteService from './services/anecdotes';
-import { setAnecdotes } from './reducers/anecdoteReducer';
+import { initializeAnecdotes } from './reducers/anecdoteReducer';
 
 const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    anecdoteService.getAll().then(anecdotes => {
-      dispatch(setAnecdotes(anecdotes));
-    })
+    dispatch(initializeAnecdotes());
   }, []);
 
   return (
